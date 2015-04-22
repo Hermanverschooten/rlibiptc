1d9c7132ed6318a30f4ee28238f8400b34efef50
Herman verschooten <Herman@verschooten.net>
2015-04-22 16:24:35 +0200

    Add chains, start of rules

--- ext/rlibiptc/rlibiptc.c
+++ ext/rlibiptc/rlibiptc.c
@@ -18,19 +18,14 @@ VALUE cClass = Qnil;
 
 struct xtc_handle *my_handle;
 bool opened = false;
-char *table;
 
-void Init_rlibiptc() {
-    cClass = rb_define_class("Iptables", rb_cObject);
-    rb_define_module_function(cClass, "initialize", method_iptc_init, 1);
-    rb_define_module_function(cClass, "close", method_iptc_close, 0);
-    rb_define_module_function(cClass, "open?", method_iptc_opened, 0);
-    rb_define_module_function(cClass, "table", method_iptc_table, 0);
-    rb_define_module_function(cClass, "rules", method_iptc_rules, 1);
-}
 
-VALUE method_iptc_init(VALUE self, VALUE tablename) {
-    table = StringValueCStr(tablename);
+void open_iptables(VALUE self) {
+    VALUE itable;
+
+    itable = rb_iv_get(self, "@table");
+
+    char *table = StringValueCStr(itable);
     if(opened)
         rb_raise(rb_eStandardError, "Iptables already opened!");
 
@@ -39,36 +34,80 @@ VALUE method_iptc_init(VALUE self, VALUE tablename) {
         rb_raise(rb_eStandardError, "Unable to initialize with table %s, %s", table, iptc_strerror(errno));
 
     opened = true;
-
-    return Qtrue;
 }
 
-VALUE method_iptc_close(VALUE self) {
+void close_iptables() {
     if(opened) {
         iptc_free(my_handle);
         opened = false;
     }
-    return Qtrue;
 }
 
-VALUE method_iptc_table(VALUE self) {
-    if(opened)
-        return rb_str_new2(table);
-    else
-        return Qnil;
+VALUE method_table(VALUE self) {
+    return rb_iv_get(self, "@table");
 }
 
-VALUE method_iptc_opened(VALUE self) {
-    return opened ? Qtrue : Qfalse;
+VALUE method_chains(VALUE self) {
+    open_iptables(self);
+
+    VALUE arr;
+
+    arr = rb_ary_new();
+
+    const char *chain = NULL;
+
+    for(chain = iptc_first_chain(my_handle); chain; chain = iptc_next_chain(my_handle)) {
+        rb_ary_push(arr, rb_str_new2(chain));
+    }
+
+    close_iptables();
+
+    return arr;
 }
 
-VALUE method_iptc_rules(VALUE self, VALUE chain_name) {
-    if(!opened)
-        rb_raise(rb_eStandardError, "You need to use open first");
-
+VALUE method_rules(VALUE self, VALUE chain_name) {
     char *chain = StringValueCStr(chain_name);
+
+    if(strlen(chain) ==0)
+        rb_raise(rb_eStandardError, "Chain is required!");
+
     const struct ipt_entry *e;
 
-    e = iptc_first_rule(chain, my_handle);
+    open_iptables(self);
 
+    VALUE arr;
 
+    arr = rb_ary_new();
+
+    int i = 0;
+
+    for(e = iptc_first_rule(chain, my_handle); e; e = iptc_next_rule(e,my_handle)) {
+        VALUE entry;
+        entry = rb_hash_new();
+        rb_hash_aset(entry, ID2SYM(rb_intern("line")), INT2NUM(i++));
+        rb_hash_aset(entry, ID2SYM(rb_intern("bytes")), LONG2NUM(e->counters.bcnt));
+        rb_hash_aset(entry, ID2SYM(rb_intern("packets")), LONG2NUM(e->counters.pcnt));
+
+        rb_ary_push(arr, entry);
+    }
+
+    close_iptables();
+
+    return arr;
+
+}
+
+VALUE method_init(VALUE self, VALUE tablename) {
+    rb_iv_set(self, "@table", tablename);
+    return self;
+}
+
+void Init_rlibiptc() {
+    VALUE Module;
+    Module = rb_define_module("Rlibiptc");
+    cClass = rb_define_class_under(Module, "Iptables", rb_cObject);
+    rb_define_method(cClass, "initialize", method_init, 1);
+    rb_define_method(cClass, "table", method_table, 0);
+    rb_define_method(cClass, "rules", method_rules, 1);
+    rb_define_method(cClass, "chains", method_chains, 0);
+}
--- ext/rlibiptc/rlibiptc.h
+++ ext/rlibiptc/rlibiptc.h
@@ -10,9 +10,4 @@
 #ifndef _RUBY_LIBIPTC_H
 #define _RUBY_LIBIPTC_H
 
-VALUE method_iptc_init(VALUE, VALUE);
-VALUE method_iptc_close(VALUE);
-VALUE method_iptc_table(VALUE);
-VALUE method_iptc_opened(VALUE);
-
 #endif
